47c122afe15a8a18d2b09937f5b87cbd7cbde1a5
William Bundy <william.tc.bundy@gmail.com>
2026-09-14 16:10:40 -0700

    updated formats

--- config/formats.ts
+++ config/formats.ts
@@ -19,14 +19,36 @@ The column value will be ignored for repeat sections.
 
 export const Formats: import('../sim/dex-formats').FormatList = [
 	{
+		section: "S/V Singles",
+	},
+	{
+		name: "[Gen 9] Random Battle",
+		desc: `Randomized teams of Pok&eacute;mon with sets that are generated to be competitively viable.`,
+		mod: 'gen9',
+		team: 'random',
+		bestOfDefault: true,
+		ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod', 'Illusion Level Mod'],
+	},
+	{
 		section: "Legacy Formats"
 	},
 	{
-		name: "[Gen 4] Legacy",
+		name: "[Gen 4] PtL Singles",
+		mod: 'gen4legacy',
+		gameType: 'singles',
+		searchShow: true,
+		bestOfDefault: true,
+		rated: false,
+		ruleset: ['Team Preview', 'Species Clause', 'Nickname Clause', 'Item Clause = 1', 'Adjust Level Down = 50', 'Picked Team Size = Auto', 'Cancel Mod'],
+		banlist: ['Restricted Legendary'],
+	},
+	{
+		name: "[Gen 4] PtL Doubles",
 		mod: 'gen4legacy',
 		gameType: 'doubles',
 		searchShow: true,
 		bestOfDefault: true,
+		rated: false,
 		ruleset: ['Team Preview', 'Species Clause', 'Nickname Clause', 'Item Clause = 1', 'Adjust Level Down = 50', 'Picked Team Size = Auto', 'Cancel Mod'],
 		banlist: ['Restricted Legendary'],
 	},
