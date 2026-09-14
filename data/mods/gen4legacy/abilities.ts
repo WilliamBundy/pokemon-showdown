03ca75815a5b25717c694b7c14fa9df785c72013
William Bundy <william.tc.bundy@gmail.com>
2026-09-14 15:45:08 -0700

    updated abilities, fixed berry inheritance, added modern mental herb

--- data/mods/gen4legacy/abilities.ts
+++ data/mods/gen4legacy/abilities.ts
@@ -1,2 +1,132 @@
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
+	// we want the modern versions of these
+	// so I've set them to inhert: false and grabbed the gen9 versions
+	illuminate: {
+		inherit: false,
+		onTryBoost(boost, target, source, effect) {
+			if (source && target === source) return;
+			if (boost.accuracy && boost.accuracy < 0) {
+				delete boost.accuracy;
+				if (!(effect as ActiveMove).secondaries) {
+					this.add('-fail', target, 'unboost', 'accuracy', '[from] ability: Illuminate', `[of] ${target}`);
+				}
+			}
+		},
+		onModifyMove(move) {
+			move.ignoreEvasion = true;
+		},
+		flags: { breakable: 1 },
+		name: "Illuminate",
+		rating: 0.5,
+		num: 35,
+	},
+	lightningrod: {
+		inherit: false
+		onTryHit(target, source, move) {
+			if (target !== source && move.type === 'Electric') {
+				if (!this.boost({ spa: 1 })) {
+					this.add('-immune', target, '[from] ability: Lightning Rod');
+				}
+				return null;
+			}
+		},
+		onAnyRedirectTarget(target, source, source2, move) {
+			if (move.type !== 'Electric' || move.flags['pledgecombo']) return;
+			const redirectTarget = ['randomNormal', 'adjacentFoe'].includes(move.target) ? 'normal' : move.target;
+			if (this.validTarget(this.effectState.target, source, redirectTarget)) {
+				if (move.smartTarget) move.smartTarget = false;
+				if (this.effectState.target !== target) {
+					this.add('-activate', this.effectState.target, 'ability: Lightning Rod');
+				}
+				return this.effectState.target;
+			}
+		},
+		flags: { breakable: 1 },
+		name: "Lightning Rod",
+		rating: 3,
+		num: 31,
+	},
+	magmaarmor: {
+		inherit:false,
+		onSourceModifyAtkPriority: 6,
+		onSourceModifyAtk(atk, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('Magma Armor weaken');
+				return this.chainModify(0.125);
+			}
+		},
+		onSourceModifySpAPriority: 5,
+		onSourceModifySpA(atk, attacker, defender, move) {
+			if (move.type === 'Water') {
+				this.debug('Magma Armor weaken');
+				return this.chainModify(0.125);
+			}
+		},	
+		flags: { breakable: 1 },
+		name: "Magma Armor",
+		rating: 0.5,
+		num: 40,
+	},
+
+	stench: {
+		inherit: true,
+		onModifyMovePriority: -1,
+		onModifyMove(move) {
+			if (move.category !== "Status") {
+				this.debug('Adding (custom, additive) Stench flinch');
+				move.secondaries.push({
+					chance: 10,
+					volatileStatus: 'flinch',
+				});
+			}
+		},	
+	},
+	stormdrain: {
+		inherit: false,
+		onTryHit(target, source, move) {
+			if (target !== source && move.type === 'Water') {
+				if (!this.boost({ spa: 1 })) {
+					this.add('-immune', target, '[from] ability: Storm Drain');
+				}
+				return null;
+			}
+		},
+		onAnyRedirectTarget(target, source, source2, move) {
+			if (move.type !== 'Water' || move.flags['pledgecombo']) return;
+			const redirectTarget = ['randomNormal', 'adjacentFoe'].includes(move.target) ? 'normal' : move.target;
+			if (this.validTarget(this.effectState.target, source, redirectTarget)) {
+				if (move.smartTarget) move.smartTarget = false;
+				if (this.effectState.target !== target) {
+					this.add('-activate', this.effectState.target, 'ability: Storm Drain');
+				}
+				return this.effectState.target;
+			}
+		},
+		flags: { breakable: 1 },
+		name: "Storm Drain",
+		rating: 3,
+		num: 114,
+	},
+	sturdy: {
+		inherit: false,
+		onTryHit(pokemon, target, move) {
+			if (move.ohko) {
+				this.add('-immune', pokemon, '[from] ability: Sturdy');
+				return null;
+			}
+		},
+		onDamagePriority: -30,
+		onDamage(damage, target, source, effect) {
+			if (target.hp === target.maxhp && damage >= target.hp && effect && effect.effectType === 'Move') {
+				this.add('-ability', target, 'Sturdy');
+				return target.hp - 1;
+			}
+		},
+		flags: { breakable: 1 },
+		name: "Sturdy",
+		rating: 3,
+		num: 5,
+	},
+
+
 };
--- data/mods/gen4legacy/items.ts
+++ data/mods/gen4legacy/items.ts
@@ -1,4 +1,45 @@
 export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
+	// modern mental herb
+	mentalherb: {
+		inherit: false
+		name: "Mental Herb",
+		spritenum: 285,
+		fling: {
+			basePower: 10,
+			effect(pokemon) {
+				const conditions = ['attract', 'taunt', 'encore', 'torment', 'disable', 'healblock'];
+				for (const firstCondition of conditions) {
+					if (pokemon.volatiles[firstCondition]) {
+						for (const secondCondition of conditions) {
+							pokemon.removeVolatile(secondCondition);
+							if (firstCondition === 'attract' && secondCondition === 'attract') {
+								this.add('-end', pokemon, 'move: Attract', '[from] item: Mental Herb');
+							}
+						}
+						return;
+					}
+				}
+			},
+		},
+		onUpdate(pokemon) {
+			const conditions = ['attract', 'taunt', 'encore', 'torment', 'disable', 'healblock'];
+			for (const firstCondition of conditions) {
+				if (pokemon.volatiles[firstCondition]) {
+					if (!pokemon.useItem()) return;
+					for (const secondCondition of conditions) {
+						pokemon.removeVolatile(secondCondition);
+						if (firstCondition === 'attract' && secondCondition === 'attract') {
+							this.add('-end', pokemon, 'move: Attract', '[from] item: Mental Herb');
+						}
+					}
+					return;
+				}
+			}
+		},
+		num: 219,
+		gen: 3,
+	},
+
 /*
 emitted by:
 cols = ['type', 70, 85, 100]
@@ -13,198 +54,264 @@ for line in berrytable.split('\n'):
         for b in berries:
             bname = b.lower().replace(' ', '')
             print(f'\t{bname}: {{')
+            print(f'\t\tinherit: true,')
             print(f'\t\tnaturalGift: {{ basePower: {cols[i]} }},')
             print('\t},')
 */
-	chilanberry: {
+		chilanberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	cheriberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	occaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	blukberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	watmelberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	chestoberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	passhoberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	nanabberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	durinberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	pechaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	wacanberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	wepearberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	belueberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	rawstberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	rindoberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	pinapberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	liechiberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	aspearberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	yacheberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	pomegberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	ganlonberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	leppaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	chopleberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	kelpsyberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	salacberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	oranberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	kebiaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	qualotberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	petayaberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	persimberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	shucaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	hondewberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	apicotberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	lumberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	cobaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	grepaberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	lansatberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	sitrusberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	payapaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	tamatoberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	starfberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	figyberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	tangaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	cornnberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	enigmaberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	wikiberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	chartiberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	magostberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	micleberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	magoberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	kasibberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	rabutaberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	custapberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	aguavberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	habanberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	nomelberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	jabocaberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	iapapaberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	colburberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	spelonberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
 	},
 	rowapberry: {
+		inherit: true,
 		naturalGift: { basePower: 100 },
 	},
 	razzberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	babiriberry: {
+		inherit: true,
 		naturalGift: { basePower: 70 },
 	},
 	pamtreberry: {
+		inherit: true,
 		naturalGift: { basePower: 85 },
-	},};
+	},
+}
